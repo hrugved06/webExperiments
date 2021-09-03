d2c4435abb4279cdd181547c6c6a39d94c2f0da8
HrugVed <geeky.hrugved143@gmail.com>
2021-09-04 03:06:12 +0530

    dev disabled for mac'

--- script.js
+++ script.js
@@ -59,6 +59,9 @@ document.onkeydown = function(e) {
   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
      return false;
   }
+  if(e.keyCode == 'S'.charCodeAt(0) && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      return false;
+  }
   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
      return false;
   }
