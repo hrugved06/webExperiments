e7631bebaad7b5a6250d58f13d9cccd185ca4994
HrugVed <geeky.hrugved143@gmail.com>
2021-09-04 02:42:57 +0530

    disable dev

--- script.js
+++ script.js
@@ -50,16 +50,16 @@ document.onkeydown = function(e) {
   if(e.keyCode == 123) {
      return false;
   }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'c'.charCodeAt(0)) {
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
      return false;
   }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'v'.charCodeAt(0)) {
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
      return false;
   }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'a'.charCodeAt(0)) {
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
      return false;
   }
-  if(e.ctrlKey && e.keyCode == 'i'.charCodeAt(0)) {
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
      return false;
   }
 }
