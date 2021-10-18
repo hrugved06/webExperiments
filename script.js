cecc38404a77f6beaecb2ce79a9a583d42e632a1
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2021-10-18 22:51:03 +0530

    dark mode 2

--- script.js
+++ script.js
@@ -71,3 +71,19 @@ function(){
         $("#favicon").attr("href","sticker2.png");
     }
 });
+
+var options = {
+    bottom: '64px', // default: '32px'
+    right: '32px', // default: 'unset'
+    left: 'unset', // default: '32px'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓' // default: ''
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
